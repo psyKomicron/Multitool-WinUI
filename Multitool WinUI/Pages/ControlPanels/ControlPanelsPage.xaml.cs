4cb849132c89c230c0c00eea1f3900c59033605b
psyKomicron <julien@cavaweb.net>
2021-09-13 10:23:39 +0200

    user can pin panels, configuration is dynamic (the app listen for changes in the file)

--- Multitool WinUI/Pages/ControlPanels/ControlPanelsPage.xaml.cs
+++ Multitool WinUI/Pages/ControlPanels/ControlPanelsPage.xaml.cs
@@ -7,8 +7,12 @@ using Multitool.FileSystem;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 using Windows.Storage;
 using Windows.System;
@@ -25,22 +29,18 @@ namespace MultitoolWinUI.Pages.ControlPanels
     /// </summary>
     public sealed partial class ControlPanelsPage : Page
     {
-        private const string settingsUri = "ms-settings:";
-        private const string startupUri = "ms-settings:startupapps";
-        private const string audioUri = "ms-settings:easeofaccess-audio";
-        private const string nightlightUri = "ms-settings:nightlight";
-        private const string soundUri = "ms-settings:sound";
-        private const string windowsUpdateUri = "ms-settings:windowsupdate";
         private const string customSettingsPathFileName = "custom_settings.xml";
 
-        private Dictionary<string, Uri> pathes = new();
+        private Dictionary<string, Tuple<Uri, bool>> pathes = new();
         private FileSystemWatcher watcher;
 
         public ControlPanelsPage()
         {
             InitializeComponent();
-            LoadCustoms();
+            Task.Run(() => LoadCustoms());
         }
+
+        #region private methods
 
         private async void LoadCustoms()
         {
@@ -53,7 +53,8 @@ namespace MultitoolWinUI.Pages.ControlPanels
             catch (FileNotFoundException)
             {
                 _ = await ApplicationData.Current.LocalFolder.CreateFileAsync(customSettingsPathFileName, CreationCollisionOption.ReplaceExisting);
-                xmlDocument.Load(path);
+                xmlDocument.AppendChild(xmlDocument.CreateElement("pathes"));
+                xmlDocument.Save(path);
                 App.MainWindow.DisplayMessage("Custom settings file created (since none was found)");
             }
             catch (XmlException e)
@@ -64,32 +65,7 @@ namespace MultitoolWinUI.Pages.ControlPanels
             }
 
             List<UIElement> frameworkElements = new();
-            XmlNode root = xmlDocument.SelectSingleNode(".//pathes");
-            if (root != null)
-            {
-                foreach (XmlNode node in root)
-                {
-                    //Debug.WriteLine(attr.Name + ": " + attr.Value);
-                    if (node.Attributes["name"].Value != null && node.Attributes["value"].Value != null && !pathes.ContainsKey(node.Attributes["name"].Value))
-                    {
-                        string name = node.Attributes["name"].Value;
-                        string value = node.Attributes["value"].Value;
-
-                        pathes.Add(name, new(value));
-
-                        Button button = new()
-                        {
-                            Content = new TextBlock()
-                            {
-                                Text = node.Attributes["name"].Value,
-                                TextWrapping = TextWrapping.WrapWholeWords
-                            }
-                        };
-                        button.Click += Button_Click;
-                        frameworkElements.Add(button);
-                    }
-                }
-            }
+            LoadNewElements(xmlDocument.DocumentElement);
 
             watcher = WatcherFactory.CreateWatcher(ApplicationData.Current.LocalFolder.Path, new()
             {
@@ -101,37 +77,329 @@ namespace MultitoolWinUI.Pages.ControlPanels
             {
                 for (int i = 0; i < frameworkElements.Count; i++)
                 {
-                    PanelsWrapGrid.Children.Add(frameworkElements[i]);
+                    ItemsWrapGrid.Children.Add(frameworkElements[i]);
                 }
             });
         }
 
-        private void OnFileChange(object sender, FileSystemEventArgs e)
+        private UIElement CreateElements(string name, Uri uri, bool isPinned)
         {
-            if (e.Name == customSettingsPathFileName)
-            {
-                Debug.WriteLine(e.ChangeType);
-            }
+            SettingPathView settingPathButton = new(name, uri, isPinned);
+            settingPathButton.Clicked += SettingPathButton_Clicked;
+            settingPathButton.Deleted += SettingPathButton_Deleted;
+            settingPathButton.Pinned += SettingPathButton_Pinned;
+            return settingPathButton;
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private SettingPathView FindButton(string name)
         {
-            if (sender is Button button)
+            UIElementCollection collection = pathes[name].Item2 ? PinnedItemsWrapGrid.Children : ItemsWrapGrid.Children;
+            for (int i = 0; i < collection.Count; i++)
             {
-                if (pathes.TryGetValue(((TextBlock)button.Content).Text, out Uri uri))
+                if ((collection[i] as SettingPathView).ButtonName == name)
                 {
-                    if (!await Launcher.LaunchUriAsync(uri))
+                    return collection[i] as SettingPathView;
+                }
+            }
+            return null;
+        }
+
+        private SettingPathView FindButton(string name, bool pinned)
+        {
+            UIElementCollection collection = pinned ? PinnedItemsWrapGrid.Children : ItemsWrapGrid.Children;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if ((collection[i] as SettingPathView).ButtonName == name)
+                {
+                    return collection[i] as SettingPathView;
+                }
+            }
+            return null;
+        }
+
+        private int FindButtonIndex(string name)
+        {
+            UIElementCollection collection = pathes[name].Item2 ? PinnedItemsWrapGrid.Children : ItemsWrapGrid.Children;
+            for (int i = 0; i < collection.Count; i++)
+            {
+                if ((collection[i] as SettingPathView).ButtonName == name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void LoadNewElements(XmlElement root)
+        {
+            // find new valid nodes
+            foreach (XmlNode node in root)
+            {
+                if (node.Name == "path")
+                {
+                    if (!string.IsNullOrWhiteSpace(node.Attributes["name"]?.Value) &&
+                        !string.IsNullOrWhiteSpace(node.Attributes["value"]?.Value))
                     {
-                        Trace.WriteLine("Unable to launch: " + uri.AbsoluteUri);
+                        string name = node.Attributes["name"].Value;
+                        string value = node.Attributes["value"].Value;
+                        bool pinned = node.Attributes["pinned"] != null && node.Attributes["pinned"]?.Value == "True";
+                        try
+                        {
+                            Uri newUri = new(value);
+                            if (pathes.ContainsKey(name))
+                            {
+                                if (pathes[name].Item1 != newUri)
+                                {
+                                    pathes[name] = new(newUri, pathes[name].Item2);
+                                }
+                                if (pathes[name].Item2 != pinned)
+                                {
+                                    _ = DispatcherQueue.TryEnqueue(() =>
+                                    {
+                                        UIElement el = FindButton(name, pathes[name].Item2);
+                                        if (!pathes[name].Item2)
+                                        {
+                                            _ = ItemsWrapGrid.Children.Remove(el);
+                                            PinnedItemsWrapGrid.Children.Add(CreateElements(name, pathes[name].Item1, pathes[name].Item2));
+                                        }
+                                        else
+                                        {
+                                            _ = PinnedItemsWrapGrid.Children.Remove(el);
+                                            ItemsWrapGrid.Children.Add(CreateElements(name, pathes[name].Item1, pathes[name].Item2));
+                                        }
+                                        pathes[name] = new(pathes[name].Item1, pinned);
+                                    });
+                                }
+                            }
+                            else
+                            {
+                                Debug.WriteLine("Adding '" + name + "'. " + (pinned ? "Pinned" : "Not pinned"));
+                                pathes.Add(name, new(newUri, pinned));
+                                if (pinned)
+                                {
+                                    _ = DispatcherQueue.TryEnqueue(() => PinnedItemsWrapGrid.Children.Add(CreateElements(name, newUri, true)));
+                                }
+                                else
+                                {
+                                    _ = DispatcherQueue.TryEnqueue(() => ItemsWrapGrid.Children.Add(CreateElements(name, newUri, false)));
+                                }
+                            }
+                        }
+                        catch (UriFormatException ex)
+                        {
+                            Trace.WriteLine(ex);
+                            App.MainWindow.DisplayMessage("Unable to parse changes from the .XML settings file.");
+                        }
                     }
                 }
             }
         }
 
+        private void RemoveOldElements(XmlElement root)
+        {
+            List<string> names = new();
+            Dictionary<string, Tuple<Uri, bool>>.KeyCollection keys = pathes.Keys;
+            foreach (string key in keys)
+            {
+                bool contains = false;
+                foreach (XmlNode node in root)
+                {
+                    if (node.Name == "path")
+                    {
+                        if (node.Attributes["name"]?.Value == key)
+                        {
+                            contains = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!contains)
+                {
+                    names.Add(key);
+                }
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+#if TRACE
+                if (pathes.Remove(names[i]))
+                {
+                    Trace.WriteLine("Removed " + names[i] + " from settings pathes");
+                }
+                else
+                {
+                    Trace.WriteLine("Unable to remove " + names[i] + " from settings pathes");
+                }
+#else
+                pathes.Remove(names[i]);
+#endif
+            }
+            _ = DispatcherQueue.TryEnqueue(() =>
+            {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    // find button to remove
+                    if (pathes[names[i]].Item2)
+                    {
+                        PinnedItemsWrapGrid.Children.Remove(FindButton(names[i]));
+                    }
+                    else
+                    {
+                        ItemsWrapGrid.Children.Remove(FindButton(names[i]));
+                    }
+                }
+            });
+        }
+
+        #endregion
+
+        #region event handlers
+
+        #region control
+
+        private async void SettingPathButton_Clicked(SettingPathView sender, RoutedEventArgs args)
+        {
+            if (!await Launcher.LaunchUriAsync(sender.SettingUri))
+            {
+                Trace.WriteLine("Unable to launch: " + sender.SettingUri.AbsoluteUri);
+            }
+        }
+
+        private void SettingPathButton_Deleted(SettingPathView sender, RoutedEventArgs args)
+        {
+            int i = FindButtonIndex(sender.ButtonName);
+            if (i != -1)
+            {
+                ItemsWrapGrid.Children.RemoveAt(i);
+            }
+        }
+
+        private void SettingPathButton_Pinned(SettingPathView sender, bool pinned)
+        {
+            //pathes[sender.ButtonName] = new(pathes[sender.ButtonName].Item1, pinned);
+            XmlDocument doc = new();
+            try
+            {
+                doc.Load(Path.Combine(ApplicationData.Current.LocalFolder.Path, customSettingsPathFileName));
+                XmlElement root = doc.DocumentElement;
+                
+                foreach (XmlNode node in root)
+                {
+                    if (node.Name == "path" && node.Attributes != null && node.Attributes["name"]?.Value == sender.ButtonName)
+                    {
+                        if (node.Attributes["pinned"] != null)
+                        {
+                            node.Attributes["pinned"].Value = pinned.ToString();
+                        }
+                        else // if the node does not have the pinned attribute i create it
+                        {
+                            XmlAttribute newAttribute = doc.CreateAttribute("pinned");
+                            newAttribute.Value = pinned.ToString();
+                            node.Attributes.Append(newAttribute);
+                        }
+                        break;
+                    }
+                }
+                doc.Save(Path.Combine(ApplicationData.Current.LocalFolder.Path, customSettingsPathFileName));
+            }
+            catch (XmlException ex)
+            {
+                Trace.WriteLine("Unable to pin/unpin element (name: " + sender.ButtonName + ").\n" + ex.ToString());
+                if (pinned)
+                {
+                    ItemsWrapGrid.Children.Remove(sender);
+                    PinnedItemsWrapGrid.Children.Add(sender);
+                }
+                else
+                {
+                    PinnedItemsWrapGrid.Children.Remove(sender);
+                    ItemsWrapGrid.Children.Add(sender);
+                }
+                App.MainWindow.DisplayMessage("Failed to save the element state (pinned/unpinned).");
+            }
+        }
+
+        #endregion
+
+        #region window
+
         private void LoadSettingsButton_Click(object sender, RoutedEventArgs e)
         {
-            pathes.Clear();
-            LoadCustoms();
+            _ = Launcher.LaunchUriAsync(new Uri(ApplicationData.Current.LocalFolder.Path));
         }
+
+        private void AddSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SettingsSearchBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            List<string> suggestions = new();
+            Regex query = new("^(" + sender.Text + ")");
+            foreach (KeyValuePair<string, Tuple<Uri, bool>> pair in pathes)
+            {
+                if (query.IsMatch(pair.Key))
+                {
+                    suggestions.Add(pair.Key);
+                }
+                if (query.IsMatch(pair.Value.Item1.AbsoluteUri))
+                {
+                    suggestions.Add(pair.Value.ToString());
+                }
+            }
+            _ = DispatcherQueue.TryEnqueue(() => sender.ItemsSource = suggestions);
+        }
+
+        private async void SettingsSearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.QueryText))
+            {
+                if (pathes.TryGetValue(args.QueryText, out Tuple<Uri, bool> tuple))
+                {
+                    if (!await Launcher.LaunchUriAsync(tuple.Item1))
+                    {
+                        Trace.WriteLine("Unable to launch: " + tuple.Item1.AbsoluteUri);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        private void OnFileChange(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name == customSettingsPathFileName)
+            {
+                try
+                {
+                    Debug.WriteLine("Parsing xml");
+                    XmlDocument doc = new();
+                    doc.Load(e.FullPath);
+                    XmlElement root = doc.DocumentElement;
+
+                    LoadNewElements(root);
+                    RemoveOldElements(root);
+                    Debug.WriteLine("Parsed xml");
+                }
+                catch (XmlException ex)
+                {
+                    Trace.WriteLine("XmlException: Unable to parse changes from the .XML settings file.\n" + ex);
+                    App.MainWindow.DisplayMessage("Unable to parse changes from the .XML settings file.");
+                }
+                catch (IOException ex)
+                {
+                    Trace.WriteLine("IOException: Unable to parse changes from the .XML settings file.\n" + ex);
+                    
+                    if (ex.HResult != -2147024864)
+                    {
+                        App.MainWindow.DisplayMessage("Unable to parse changes from the .XML settings file.");
+                    }
+                }
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Multitool WinUI/Pages/ControlPanels/SettingPathView.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+using System;
+
+using Windows.Foundation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace MultitoolWinUI.Pages.ControlPanels
+{
+    public sealed partial class SettingPathView : UserControl
+    {
+        public SettingPathView()
+        {
+            InitializeComponent();
+        }
+
+        public SettingPathView(string name, Uri uri, bool isPinned)
+        {
+            InitializeComponent();
+            SettingUri = uri;
+            ButtonName = name;
+            IsPinned = isPinned;
+            PinButton.Text = IsPinned ? "Unpin" : "Pin";
+        }
+
+        #region events
+
+        /// <summary>
+        /// Occurs when the item is deleted.
+        /// </summary>
+        public event TypedEventHandler<SettingPathView, RoutedEventArgs> Deleted;
+
+        /// <summary>
+        /// Occurs when the control is clicked.
+        /// </summary>
+        public event TypedEventHandler<SettingPathView, RoutedEventArgs> Clicked;
+
+        /// <summary>
+        /// Occurs when the control is pinned. True if the control has been pinned, false if the control has be unpinned.
+        /// </summary>
+        public event TypedEventHandler<SettingPathView, bool> Pinned;
+
+        #endregion
+
+        #region properties
+
+        public string ButtonName { get; set; }
+
+        public Uri SettingUri { get; set; }
+
+        public bool IsPinned { get; set; }
+
+        #endregion
+
+        #region event handlers
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Deleted?.Invoke(this, e);
+        }
+
+        private void PinButton_Click(object sender, RoutedEventArgs e)
+        {
+            IsPinned = !IsPinned;
+            PinButton.Text = IsPinned ? "Unpin" : "Pin";
+            Pinned?.Invoke(this, IsPinned);
+        }
+
+        private void ControlButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clicked?.Invoke(this, e);
+        }
+
+        #endregion
+    }
+}
