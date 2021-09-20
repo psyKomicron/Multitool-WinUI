faef13b46e28cfa2503f648577c1fa269449b610
psyKomicron <julien@cavaweb.net>
2021-09-20 22:14:18 +0200

    added SettingName property

--- Multitool WinUI/Helpers/SettingNotFoundException.cs
+++ Multitool WinUI/Helpers/SettingNotFoundException.cs
@@ -4,6 +4,11 @@ namespace MultitoolWinUI.Helpers
 {
     public class SettingNotFoundException : Exception
     {
-        public SettingNotFoundException(string settingName, string message = "Setting not found") : base(message + ". Setting name: " + settingName) { }
+        public SettingNotFoundException(string settingName, string message = "Setting not found") : base(message + ". Setting name: " + settingName)
+        {
+            SettingName = settingName;
+        }
+
+        public string SettingName { get; }
     }
 }
