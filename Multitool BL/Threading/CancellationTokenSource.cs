fd68fe36d79fd2caf6357b34e6055315cdd0c630
psyKomicron <psykomicron@protonmail.com>
2021-10-23 14:56:00 +0200

    created a cancellation token wich allows handle when the token in cancelled (event raised by the method using the token)

---
+++ Multitool BL/Threading/CancellationTokenSource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+using Windows.Foundation;
+
+namespace Multitool.Threading
+{
+    public class ListenableCancellationTokenSource : CancellationTokenSource
+    {
+        public event TypedEventHandler<ListenableCancellationTokenSource, EventArgs> Cancelled;
+
+        public void InvokeCancel()
+        {
+            if (Token.IsCancellationRequested)
+            {
+                Cancelled?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
