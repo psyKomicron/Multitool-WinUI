ef5654156ff031f7578de68df384efa20f65f8c7
psyKomicron <psykomicron@protonmail.com>
2021-11-09 20:54:34 +0100

    added documentation

--- MultitoolDAL/FileSystem/Events/ChangeTypes.cs
+++ MultitoolDAL/FileSystem/Events/ChangeTypes.cs
@@ -2,13 +2,31 @@
 
 namespace Multitool.DAL.FileSystem.Events
 {
+    /// <summary>
+    /// Change types.
+    /// </summary>
     [Flags]
     public enum ChangeTypes
     {
+        /// <summary>
+        /// When a directory in watched path has been created.
+        /// </summary>
         DirectoryCreated,
+        /// <summary>
+        /// When a file in watched path has been created.
+        /// </summary>
         FileCreated,
+        /// <summary>
+        /// When a file in a watched path has been deleted.
+        /// </summary>
         FileDeleted,
+        /// <summary>
+        /// When a watched path has been deleted.
+        /// </summary>
         PathDeleted,
+        /// <summary>
+        /// When nothing has been done.
+        /// </summary>
         None
     }
 }
