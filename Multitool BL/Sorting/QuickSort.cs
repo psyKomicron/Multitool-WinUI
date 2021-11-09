ef76140300a4b2dd561aea9ec397c89e9b0f7f75
psyKomicron <psykomicron@protonmail.com>
2021-11-09 20:31:03 +0100

    refactors to use IComparer object to sort instead of using IComparable objects (more flexibility)

--- Multitool BL/Sorting/QuickSort.cs
+++ Multitool BL/Sorting/QuickSort.cs
@@ -1,17 +1,96 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Multitool.Sorting
 {
-    public class QuickSort
+    public static class QuickSort
     {
-        static void Swap<T>(T[] array, int i, int j)
+        public static void Sort<T>(T[] array, int low, int high) where T : IComparable<T>
+        {
+            CheckParameters(array, low, high);
+
+            if (low < high)
+            {
+                int pIndex = Partition(array, low, high);
+
+                Sort(array, low, pIndex - 1);
+                Sort(array, pIndex + 1, high);
+            }
+        }
+
+        public static void Sort<T>(T[] array, IComparer<T> comparer, int low, int high)
+        {
+            CheckParameters(array, low, high);
+
+            if (low < high)
+            {
+                int pIndex = PartitionWithComparer(array, comparer, low, high);
+
+                Sort(array, comparer, low, pIndex - 1);
+                Sort(array, comparer, pIndex + 1, high);
+            }
+        }
+
+        /// <summary>
+        /// Sorts the array and returns a tuple of : old index, new index, object
+        /// for each item in the <paramref name="array"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        /// <returns></returns>
+        public static List<Tuple<int, int>> SortIndexes<T>(T[] array, int low, int high) where T : class, IComparable<T>
+        {
+            CheckParameters(array, low, high);
+            List<Tuple<int, int>> indexes = new(array.Length + 1);
+            T[] copy = new T[array.Length];
+            array.CopyTo(copy, 0);
+            Sort(array, low, high);
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                int indexOf = -1;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (copy[i] == array[j])
+                    {
+                        indexOf = j;
+                        break;
+                    }
+                }
+                indexes.Add(new(i, indexOf));
+            }
+            indexes.TrimExcess();
+            return indexes;
+        }
+
+        private static void Swap<T>(T[] array, int i, int j)
         {
             T item = array[i];
             array[i] = array[j];
             array[j] = item;
         }
 
-        public static int Partition<T>(T[] array, int low, int high) where T : IComparable<T>
+        private static int PartitionWithComparer<T>(T[] array, IComparer<T> comparer, int low, int high)
+        {
+            T pivot = array[high];
+            int i = low - 1;
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (comparer.Compare(array[j], pivot) < 0)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private static int Partition<T>(T[] array, int low, int high) where T : IComparable<T>
         {
             T pivot = array[high];
             int i = low - 1;
@@ -27,9 +106,14 @@ namespace Multitool.Sorting
             return i + 1;
         }
 
-        public static void Sort<T>(T[] array, int low, int high) where T : IComparable<T>
+        /// <summary>
+        /// bounds checks && null array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="low"></param>
+        /// <param name="high"></param>
+        private static void CheckParameters<T>(T[] array, int low, int high)
         {
-            #region bounds checks && null array
             if (high >= array.Length)
             {
                 throw new ArgumentOutOfRangeException(nameof(high));
@@ -42,14 +126,10 @@ namespace Multitool.Sorting
             {
                 throw new ArgumentException("Array was null", nameof(array));
             }
-            #endregion
 
-            if (low < high)
+            if (high == -1)
             {
-                int pIndex = Partition(array, low, high);
-
-                Sort(array, low, pIndex - 1);
-                Sort(array, pIndex + 1, high);
+                high = array.Length - 1;
             }
         }
     }
