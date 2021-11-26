c20766db417087e1ea60542d271014680626e434
Julien Marquet <psykomicron@protonmail.com>
2021-11-26 12:02:57 +0100

    renamed files & added features & ui tweaks
    added features :
     - token validation (through twitch's api)
    
    ui tweaks:
     - token is now hosted in a password box
     - toggle button to request tags and auto load webview when joining chat

--- Multitool WinUI/Pages/Irc/TwitchChatPage.xaml.cs
+++ Multitool WinUI/Pages/Irc/TwitchChatPage.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.UI.Xaml.Controls;
 
 using Multitool.DAL;
 using Multitool.Net.Twitch;
+using Multitool.Net.Twitch.Irc;
+using Multitool.Net.Twitch.Security;
 
 using MultitoolWinUI.Pages.Irc;
 
@@ -10,6 +12,10 @@ using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using Windows.Web.Http;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -19,20 +25,20 @@ namespace MultitoolWinUI.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class IrcChatPage : Page
+    public sealed partial class TwitchChatPage : Page
     {
         private const string twitchUrl = "https://twitch.tv/";
         private readonly object _lock = new();
         private bool saved;
 
-        public IrcChatPage()
+        public TwitchChatPage()
         {
             InitializeComponent();
             try
             {
                 ISettings settings = App.Settings;
-                Login = settings.GetSetting<string>(nameof(IrcChatPage), nameof(Login));
-                LastStream = settings.GetSetting<string>(nameof(IrcChatPage), nameof(LastStream));
+                Login = settings.GetSetting<string>(nameof(TwitchChatPage), nameof(Login));
+                LastStream = settings.GetSetting<string>(nameof(TwitchChatPage), nameof(LastStream));
             }
             catch (SettingNotFoundException) 
             {
@@ -58,8 +64,8 @@ namespace MultitoolWinUI.Pages
             if (!saved)
             {
                 ISettings settings = App.Settings;
-                settings.SaveSetting(nameof(IrcChatPage), nameof(LastStream), LastStream);
-                settings.SaveSetting(nameof(IrcChatPage), nameof(Login), Login);
+                settings.SaveSetting(nameof(TwitchChatPage), nameof(LastStream), LastStream);
+                settings.SaveSetting(nameof(TwitchChatPage), nameof(Login), Login);
                 saved = true;
             }
         }
@@ -89,6 +95,31 @@ namespace MultitoolWinUI.Pages
         {
             SavePage();
         }
+
+        private async Task<bool> ValidateToken()
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new("Bearer", Login);
+            HttpResponseMessage res = await client.GetAsync(new(@"https://id.twitch.tv/oauth2/validate"));
+            if (res.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                Trace.TraceError("Token to connect to Twitch is invalid, create another one to be able to connect chats.");
+                return false;
+            }
+#if false
+            JsonElement value;
+            JsonDocument json = JsonDocument.Parse(await res.Content.ReadAsStringAsync());
+            if (json.RootElement.TryGetProperty("login", out value) && value.ValueKind.HasFlag(JsonValueKind.String))
+            {
+                Debug.WriteLine(value.ToString());
+            }
+            if (json.RootElement.TryGetProperty("client_id", out value))
+            {
+                Debug.WriteLine(value.ToString());
+            }
+#endif
+            return true;
+        }
         #endregion
 
         #region event handlers
@@ -96,7 +127,7 @@ namespace MultitoolWinUI.Pages
         {
             try
             {
-                LastStream = App.Settings.GetSetting<string>(nameof(IrcChatPage), nameof(LastStream));
+                LastStream = App.Settings.GetSetting<string>(nameof(TwitchChatPage), nameof(LastStream));
                 NavigateTo(LastStream);
             }
             catch (SettingNotFoundException ex)
@@ -115,8 +146,17 @@ namespace MultitoolWinUI.Pages
             LastStream = args.QueryText;
         }
 
-        private void Chats_AddTabButtonClick(TabView sender, object args)
+        private async void Chats_AddTabButtonClick(TabView sender, object args)
         {
+            if (!await ValidateToken())
+            {
+                return;
+            }
+
+            bool requestTags, loadWebView;
+            requestTags = RequestTagsButton.IsOn;
+            loadWebView = LoadWebViewButton.IsOn;
+
             if (string.IsNullOrEmpty(Login))
             {
                 Trace.TraceWarning("Cannot connect to any chat without login");
@@ -125,22 +165,27 @@ namespace MultitoolWinUI.Pages
             {
                 try
                 {
-                    TabViewItem tab = new()
-                    {
-                        Header = LastStream
-                    };
                     IIrcClient client = new TwitchIrcClient(new TwitchConnectionToken(Login))
                     {
                         NickName = "psykomicron",
                         Encoding = Encoding.UTF8,
-                        RequestTags = false
+                        RequestTags = requestTags,
+                    };
+
+                    TabViewItem tab = new()
+                    {
+                        Header = LastStream
                     };
                     Frame frame = new();
-
                     tab.Content = frame;
                     frame.Navigate(typeof(ChatPage), new ChatPageParameter(client, tab, LastStream));
                     sender.TabItems.Add(tab);
                     sender.SelectedItem = tab;
+
+                    if (loadWebView)
+                    {
+                        PageWebView.Source = new(twitchUrl + LastStream);
+                    }
                 }
                 catch (FormatException ex)
                 {
@@ -150,6 +195,26 @@ namespace MultitoolWinUI.Pages
         }
 
         private void Chats_TabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args) => Chats.TabItems.Remove(args.Tab);
+
+        private async void LoadOAuth_Click(object sender, RoutedEventArgs e)
+        {
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new("Bearer", Login);
+            HttpResponseMessage res = await client.GetAsync(new(@"https://id.twitch.tv/oauth2/validate"));
+            if (res.StatusCode == HttpStatusCode.Ok)
+            {
+                JsonDocument json = JsonDocument.Parse(await res.Content.ReadAsStringAsync());
+                if (json.RootElement.TryGetProperty("client_id", out JsonElement value))
+                {
+                    Debug.WriteLine(value.ToString());
+                    PageWebView.Source = new(@"https://id.twitch.tv/oauth2/authorize?client_id" + value.ToString() + @"=&redirect_uri=http://localhost&scope&response_type=token&scope=");
+                }
+            }
+            else
+            {
+
+            }
+        }
         #endregion
     }
 }
