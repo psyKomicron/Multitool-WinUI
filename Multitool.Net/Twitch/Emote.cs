d7efb8b8d55f99f0cb4eaf091a9f0da4c0e4c819
psykomicron <psykomicron@protonmail.com>
2021-11-30 12:48:27 +0100

    implemented emote fetcher

---
+++ Multitool.Net/Twitch/Emote.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Multitool.Net.Twitch
+{
+    public class Emote
+    {
+        public Emote() { }
+
+        public Emote(int id, string name, byte[] image)
+        {
+            Id = id;
+            Name = name;
+            NameRegex = new(name);
+            Image = image;
+        }
+
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public Regex NameRegex { get; init; }
+        public byte[] Image { get; init; }
+    }
+}
--- Multitool.Net/Twitch/EmoteFetcher.cs
+++ Multitool.Net/Twitch/EmoteFetcher.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml.Controls;
+
+using Multitool.Net.Twitch.Security;
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Windows.Web.Http;
+
 namespace Multitool.Net.Twitch
 {
-    public class EmoteFetcher
+    /// <summary>
+    /// Gets emotes from Twitch's API
+    /// </summary>
+    public class EmoteFetcher : IDisposable
     {
+        private readonly HttpClient client = new();
+        private bool disposed;
+        private readonly TwitchConnectionToken token;
 
+        public EmoteFetcher(TwitchConnectionToken connectionToken)
+        {
+            token = connectionToken;
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+            disposed = true;
+        }
+
+        public async Task<List<Emote>> GetAllEmotes()
+        {
+            CheckIfDisposed();
+
+            client.DefaultRequestHeaders.Authorization = new("Bearer", token.Token);
+            client.DefaultRequestHeaders.Add(new("Client-Id", token.ClientId));
+
+            using HttpResponseMessage response = await client.GetAsync(new(Properties.Resources.TwitchApiGlobalEmotesEndPoint), HttpCompletionOption.ResponseHeadersRead);
+
+            response.EnsureSuccessStatusCode();
+            client.DefaultRequestHeaders.Clear();
+
+            var emotes = await response.Content.ReadAsStringAsync();
+
+            Debug.WriteLine($"Emotes payload length {emotes.Length}");
+
+            return null;
+        }
+
+        private void CheckIfDisposed()
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
     }
 }
