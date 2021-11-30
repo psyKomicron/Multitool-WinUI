e1e0a3172090f525c069b722929a1ac8e03f4e63
psykomicron <psykomicron@protonmail.com>
2021-11-30 12:50:09 +0100

    change Message property type to FrameworkElement

--- Multitool WinUI/Models/ChatMessageModel.cs
+++ Multitool WinUI/Models/ChatMessageModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 
 using Multitool.Net.Twitch;
@@ -9,12 +10,7 @@ namespace MultitoolWinUI.Models
     {
         private const string dateTimeFormat = "t";
 
-        public ChatMessageModel()
-        {
-            Timestamp = string.Empty;
-            Message = string.Empty;
-            UserName = string.Empty;
-        }
+        public ChatMessageModel() { }
 
         public ChatMessageModel(Message message)
         {
@@ -23,16 +19,17 @@ namespace MultitoolWinUI.Models
             Message = new TextBlock()
             {
                 Text = message.ToString(),
-                HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Stretch,
-                TextWrapping = Microsoft.UI.Xaml.TextWrapping.WrapWholeWords,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                TextWrapping = TextWrapping.WrapWholeWords,
                 IsTextSelectionEnabled = true
             };
             NameColor = new(message.Author.NameColor);
         }
 
         public string Timestamp { get; set; }
-        public object Message { get; set; }
+        public FrameworkElement Message { get; set; }
         public string UserName { get; set; }
         public SolidColorBrush NameColor { get; set; }
+        public Image UserBadge { get; set; }
     }
 }
