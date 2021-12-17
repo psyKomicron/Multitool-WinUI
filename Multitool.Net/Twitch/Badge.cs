34c318a4ad2ff991dfe65865711f8cb462b53fc2
psykomicron <psykomicron@protonmail.com>
2021-12-17 02:58:32 +0100

    created Badge.cs

---
+++ Multitool.Net/Twitch/Badge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Multitool.Net.Twitch
+{
+    public class Badge
+    {
+        public Badge() { }
+
+        public Badge(string name, string length)
+        {
+            Name = name;
+            if (int.TryParse(length, out int n))
+            {
+                Length = n;
+            }
+            else
+            {
+                Length = 0;
+            }
+        }
+
+        public Badge(ReadOnlySpan<char> name, ReadOnlySpan<char> length)
+        {
+            Name = name.ToString();
+            if (int.TryParse(length, out int n))
+            {
+                Length = n;
+            }
+            else
+            {
+                Length = 0;
+            }
+        }
+
+        public string Name { get; set; }
+        public int Length { get; set; }
+    }
+}
