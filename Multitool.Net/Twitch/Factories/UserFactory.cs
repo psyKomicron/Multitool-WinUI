859dafe9f68d142eb8592a933fd816fae70da8d8
psykomicron <psykomicron@protonmail.com>
2021-12-17 03:04:57 +0100

    factory now parses messages
    - builds users with :
     - display name
     - client id
     - name color
     - badges
     - mod

--- Multitool.Net/Twitch/Factories/UserFactory.cs
+++ Multitool.Net/Twitch/Factories/UserFactory.cs
@@ -1,27 +1,99 @@
-﻿using System;
+﻿using Multitool.Net.Twitch.IrcTags;
+using Multitool.UI;
+
+using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Multitool.Net.Twitch.Factories
 {
     internal class UserFactory
     {
-        private static readonly ConcurrentDictionary<string, User> cache = new();
+        private readonly ConcurrentDictionary<string, User> cache = new();
+        private readonly SemaphoreSlim semaphore = new(1);
+        private readonly ColorConverter colorConverter = new(0xFF);
 
-        public User GetUser(string userName, Span<char> message)
+        public User GetUser(Memory<char> message)
         {
-            if (!cache.ContainsKey(userName))
+            if (TagParser.TryGetTag(message, "user-id", out string userId))
             {
-                User user = new(userName)
+                Dictionary<string, string> tags = TagParser.Parse(message);
+
+                if (cache.ContainsKey(userId))
                 {
-                    DisplayName = userName,
-                };
-                cache[userName] = user;
-                return user;
+                    User user = cache[userId];
+                    AssertSame(user, tags);
+                    return user;
+                }
+                else
+                {
+                    if (semaphore.Wait(10))
+                    {
+                        cache.AddOrUpdate(userId, BuildUser(tags), UpdateCallback);
+                        semaphore.Release();
+                    }
+                }
             }
-            else
+            return null;
+        }
+
+        private void AssertSame(User user, Dictionary<string, string> tags)
+        {
+
+        }
+
+        private User BuildUser(Dictionary<string, string> tags)
+        {
+            User user = new()
             {
-                return cache[userName];
+                DisplayName = tags["display-name"] == string.Empty ? "anonymous" : tags["display-name"],
+                Id = tags["user-id"],
+                IsMod = tags["mod"] == "1",
+                NameColor = colorConverter.ConvertFromString(tags["color"]),
+                Badges = new()
+            };
+            // badges
+            string badgeTag = tags["badges"];
+            if (badgeTag != string.Empty)
+            {
+                ReadOnlySpan<char> badges = new(badgeTag.ToCharArray());
+                int length = 0;
+                for (int i = 0; i < badges.Length; i++)
+                {
+                    if (badges[i] == '/')
+                    {
+                        ReadOnlySpan<char> badgeName = badges.Slice(i - length, length);
+                        length = 0;
+                        
+                        for (i += 1; i < badges.Length; i++)
+                        {
+                            if (badges[i] == ',')
+                            {
+                                ReadOnlySpan<char> badgeValue = badges.Slice(i - length, length);
+
+                                user.Badges.Add(new(badgeName, badgeValue));
+                            }
+                            else
+                            {
+                                length++;
+                            }
+                        }
+                        length = 0;
+                    }
+                    else
+                    {
+                        length++;
+                    }
+                }
             }
+            return user;
+        }
+
+        private User UpdateCallback(string key, User user)
+        {
+            return user;
         }
     }
 }
