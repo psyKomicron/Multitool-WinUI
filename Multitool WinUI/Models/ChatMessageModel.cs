4480177120f09ba287ea44422ceb152645ce3128
Julien Marquet <psykomicron@protonmail.com>
2021-12-25 00:07:19 +0100

    simplified model (work is now done by the ChatControl) *don't mind the regex*

--- Multitool WinUI/Models/ChatMessageModel.cs
+++ Multitool WinUI/Models/ChatMessageModel.cs
@@ -1,29 +1,39 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 using Multitool.Net.Twitch;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MultitoolWinUI.Models
 {
     public sealed class ChatMessageModel
     {
         private const string dateTimeFormat = "t";
+        private static readonly Regex linkRegex = new(@"(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])", RegexOptions.Multiline | RegexOptions.Compiled);
 
         public ChatMessageModel() { }
 
         public ChatMessageModel(Message message)
         {
             Timestamp = message.ServerTimestamp.ToString(dateTimeFormat);
-            UserName = message.Author.DisplayName;
+            UserName = message?.Author?.DisplayName;
+
             Message = new TextBlock()
             {
-                Text = message.ToString(),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
                 TextWrapping = TextWrapping.WrapWholeWords,
                 IsTextSelectionEnabled = true
             };
-            NameColor = new(message.Author.NameColor);
+
+            if (message.Author != null)
+            {
+                UserName = message.Author.DisplayName;
+                NameColor = new(message.Author.NameColor);
+            }
         }
 
         public string Timestamp { get; set; }
