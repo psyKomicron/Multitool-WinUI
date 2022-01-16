883fbf1896c381a33755f9ef249f35004fa3c4f1
psykomicron <psykomicron@protonmail.com>
2022-01-16 04:06:48 +0100

    added ctor(int)

--- Multitool.Net/Twitch/Irc/Id.cs
+++ Multitool.Net/Twitch/Irc/Id.cs
@@ -3,10 +3,18 @@
     public struct Id
     {
         private readonly string stringId;
+        private readonly int id;
 
         public Id(string id)
         {
             stringId = id;
+            this.id = default;
+        }
+
+        public Id(int id)
+        {
+            stringId = null;
+            this.id = id;
         }
 
         public string StringId => stringId;
