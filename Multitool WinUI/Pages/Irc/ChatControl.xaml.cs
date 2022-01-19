86d1983eb84e889ca382d8cea448f90ccb5ed36b
psykomicron <psykomicron@protonmail.com>
2022-01-19 23:38:54 +0100

    added using

--- Multitool WinUI/Pages/Irc/ChatControl.xaml.cs
+++ Multitool WinUI/Pages/Irc/ChatControl.xaml.cs
@@ -19,6 +19,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Text;
 using System.Threading.Tasks;
 
 using Windows.UI;
