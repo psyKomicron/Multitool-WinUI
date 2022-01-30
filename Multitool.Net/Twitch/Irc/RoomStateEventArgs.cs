d7d1760f2b818bd5777482a23660f7965eb60a09
psykomicron <psykomicron@protonmail.com>
2022-01-31 00:18:27 +0100

    Create RoomStateEventArgs.cs

---
+++ Multitool.Net/Twitch/Irc/RoomStateEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Multitool.Net.Twitch.Irc
+{
+    public class RoomStateEventArgs
+    {
+        private Dictionary<RoomStates, int> _data;
+
+        public RoomStateEventArgs() { }
+
+        public RoomStates States { get; internal set; }
+        public Dictionary<RoomStates, int> Data
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    _data = new();
+                }
+                return _data;
+            }
+            internal set => _data = value;
+        }
+    }
+}
