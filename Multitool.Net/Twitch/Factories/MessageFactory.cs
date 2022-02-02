a2c2a91bfd5203b32fe97d4bf9abdcfe41148b72
psykomicron <psykomicron@protonmail.com>
2022-02-02 08:03:35 +0100

    renamed CreateUser to GetOrCreateUser, added USERNOTICE factory method

--- Multitool.Net/Twitch/Factories/MessageFactory.cs
+++ Multitool.Net/Twitch/Factories/MessageFactory.cs
@@ -1,6 +1,9 @@
 ﻿
+using Multitool.Net.Twitch.Irc;
+
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Multitool.Net.Twitch.Factories
 {
@@ -9,6 +12,7 @@ namespace Multitool.Net.Twitch.Factories
         private readonly UserFactory userFactory = new();
 
         public bool UseLocalTimestamp { get; set; }
+        internal UserFactory UserFactory => userFactory;
 
         public Message CreateMessage(ReadOnlyMemory<char> memory)
         {
@@ -32,35 +36,100 @@ namespace Multitool.Net.Twitch.Factories
 
             ReadOnlySpan<char> text = data[index..];
 
-            User author = userFactory.CreateUser(tags);
+            User author = userFactory.GetOrCreateUser(tags);
             Message message = new(text.ToString())
             {
-                Author = author
+                Author = author,
+                ServerTimestamp = GetTimeStamp(tags)
             };
 
-            if (UseLocalTimestamp)
+            return message;
+        }
+
+        public static UserNoticeEventArgs CreateUserNotice(ReadOnlyMemory<char> memory, Dictionary<string, string> tags, int index)
+        {
+            UserNoticeEventArgs notice = new();
+
+            ReadOnlyMemory<char> remains = memory[index..];
+            index = 1;
+            for (; index < remains.Length; index++)
             {
-                message.ServerTimestamp = DateTime.Now;
+                if (remains.Span[index] == ':')
+                {
+                    index++;
+                    break;
+                }
             }
-            else
+            if (tags.TryGetValue("system-msg", out string systemMessage))
             {
-                try
+                int i = 0;
+                StringBuilder cleanedMessage = new();
+                while (i < systemMessage.Length)
                 {
-                    long epoch = long.Parse(tags["tmi-sent-ts"]);
-                    DateTimeOffset result = DateTimeOffset.FromUnixTimeMilliseconds(epoch);
-                    message.ServerTimestamp = result.LocalDateTime;
+                    if (systemMessage[i] == '\\')
+                    {
+                        if (i + 1 < systemMessage.Length && systemMessage[i + 1] == 's')
+                        {
+                            cleanedMessage.Append(' ');
+                            i += 2;
+                        }
+                        else
+                        {
+                            cleanedMessage.Append('\\');
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        cleanedMessage.Append(systemMessage[i]);
+                        i++;
+                    }
                 }
-                catch (FormatException)
-                {
-                    message.ServerTimestamp = DateTime.Now;
-                }
-                catch (ArgumentException)
-                {
-                    message.ServerTimestamp = DateTime.Now;
-                }
+                notice.SystemMessage = cleanedMessage.ToString();
+            }
+            notice.Message = remains[index..].ToString();
+            string noticeType = tags["msg-id"];
+            switch (noticeType) 
+            {
+                case "sub":
+                    notice.NoticeType = NoticeType.Sub;
+                    break;
+                case "resub":
+                    notice.NoticeType = NoticeType.ReSub;
+                    break;
+                case "subgift":
+                    notice.NoticeType = NoticeType.SubGift;
+                    break;
+                case "anonsubgift":
+                    notice.NoticeType = NoticeType.AnonSubGift;
+                    break;
+                case "submysterygift":
+                    notice.NoticeType = NoticeType.SubMisteryGift;
+                    break;
+                case "giftpaidupgrade":
+                    notice.NoticeType = NoticeType.GiftPaidUpgrade;
+                    break;
+                case "rewardgift":
+                    notice.NoticeType = NoticeType.RewardGift;
+                    break;
+                case "anongiftpaidupgrade":
+                    notice.NoticeType = NoticeType.AnonGiftPaidUpgrade;
+                    break;
+                case "raid":
+                    notice.NoticeType = NoticeType.Raid;
+                    break;
+                case "unraid":
+                    notice.NoticeType = NoticeType.UnRaid;
+                    break;
+                case "ritual":
+                    notice.NoticeType = NoticeType.Ritual;
+                    break;
+                case "bitsbadgetier":
+                    notice.NoticeType = NoticeType.BitsBadgeTier;
+                    break;
             }
 
-            return message;
+            return notice;
         }
 
         public static Dictionary<string, string> ParseTags(ReadOnlyMemory<char> memory, ref int index)
@@ -94,6 +163,31 @@ namespace Multitool.Net.Twitch.Factories
                 index++;
             }
             return tags;
+        }
+
+        private DateTime GetTimeStamp(Dictionary<string, string> tags)
+        {
+            if (UseLocalTimestamp)
+            {
+                return DateTime.Now;
+            }
+            else
+            {
+                try
+                {
+                    long epoch = long.Parse(tags["tmi-sent-ts"]);
+                    DateTimeOffset result = DateTimeOffset.FromUnixTimeMilliseconds(epoch);
+                    return result.LocalDateTime;
+                }
+                catch (FormatException)
+                {
+                    return DateTime.Now;
+                }
+                catch (ArgumentException)
+                {
+                    return DateTime.Now;
+                }
+            }
         }
     }
 }
