660e8e0a29fc4cc2baa2fa381c749799ec3e3c71
psykomicron <psykomicron@protonmail.com>
2022-02-18 02:16:08 +0100

    created classes to create web embeds

---
+++ Multitool.Net/Embeds/YoutubeEmbedFetcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Windows.Web.Http;
+
+namespace Multitool.Net.Embeds
+{
+    public class YoutubeEmbedFetcher : IEmbedFetcher
+    {
+        private static readonly Regex youtubeRegex = new(@"^https://youtube\.[a-z]+");
+        private readonly HttpClient client = new();
+
+        public async Task<object> Fetch(string url)
+        {
+            if (youtubeRegex.IsMatch(url))
+            {
+                var response = await client.GetAsync(new(url));
+                response.EnsureSuccessStatusCode();
+                
+            }
+            return null;
+        }
+    }
+}
