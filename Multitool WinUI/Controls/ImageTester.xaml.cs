d402fa85fd60443307b9159a1c14bff751e9956a
Julien Marquet <psykomicron@protonmail.com>
2022-03-16 23:26:18 +0100

    control update

--- Multitool WinUI/Controls/ImageTester.xaml.cs
+++ Multitool WinUI/Controls/ImageTester.xaml.cs
@@ -20,7 +20,7 @@ using Windows.Foundation.Collections;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace MultitoolWinUI.Pages.Test
+namespace MultitoolWinUI.Controls
 {
     public sealed partial class ImageTester : UserControl
     {
