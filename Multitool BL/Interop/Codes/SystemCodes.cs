b3ae004934e10c548f55a6de04a2c49a50b3ce2a
Julien Marquet <psykomicron@protonmail.com>
2022-03-18 15:45:27 +0100

    shutdown support for PowerController

--- Multitool BL/Interop/Codes/SystemCodes.cs
+++ Multitool BL/Interop/Codes/SystemCodes.cs
@@ -1,9 +1,10 @@
 ﻿namespace Multitool.Interop.Codes
 {
-    internal enum SystemCodes : uint
+    internal static class SystemCodes
     {
-        ERROR_SUCCESS = 0,
-        ERROR_ACCESS_DENIED = 5,
-        ERROR_INVALID_ACCESS = 12
+        public const uint ERROR_SUCCESS = 0;
+        public const uint ERROR_ACCESS_DENIED = 5;
+        public const uint ERROR_INVALID_ACCESS = 12;
+        public const uint ERROR_SHUTDOWN_USERS_LOGGED_ON = 1191;
     }
 }
--- Multitool BL/Interop/InteropHelper.cs
+++ Multitool BL/Interop/InteropHelper.cs
@@ -1,18 +1,32 @@
 ﻿using System.ComponentModel;
 using System.Runtime.InteropServices;
 
+using static Multitool.Interop.Codes.SystemCodes;
+
 namespace Multitool.Interop
 {
     internal static class InteropHelper
     {
         public static Win32Exception GetLastError(string message, uint funcRetCode)
         {
-            return new(Marshal.GetLastWin32Error(), message + ". (return code " + funcRetCode + ")");
+            return new(Marshal.GetLastWin32Error(), $"{message}. (HRESULT: {funcRetCode})");
         }
 
         public static Win32Exception GetLastError(string message)
         {
             return new(Marshal.GetLastWin32Error(), message);
+        }
+
+        public static Win32Exception GetLastError(uint funcRetCode)
+        {
+            string message = funcRetCode switch
+            {
+                ERROR_ACCESS_DENIED => "Access is denied.",
+                ERROR_INVALID_ACCESS => "The access code is invalid.",
+                ERROR_SHUTDOWN_USERS_LOGGED_ON => "Shutdown is not possible, other users are logged on",
+                _ => "Function returned non-zero code.",
+            };
+            return GetLastError(message, funcRetCode);
         }
     }
 }
